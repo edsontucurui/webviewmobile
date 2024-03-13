a91440a46dbf9c37ccbb7a068cf391bf1e377375
edsontucurui <edsonsilvabsb@yahoo.com.br>
2024-03-13 10:11:42 -0300

    ajuste do código do Authorization

--- src/realizarRequest.js
+++ src/realizarRequest.js
@@ -2,23 +2,24 @@ function realizarRequest() {
   const instanciaApp = '1';
   const chavePasse = document.getElementById('chavePasseInput').value;
   const chaveFuncionalidade = '731bd214-9de0-4b0c-9d63-e549296552f3';
-  const Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlzIjoiY2hhdmVQYXNzZSIsImtleSI6ImE2OTIyZWRmLWE3ZTQtNGZjYy04NDlhLTQxNTUxMDg0NDRhMSIsImlhdCI6MTcxMDMzMjA0NCwiZXhwIjozMjg4MjEyMDQ0LCJhdWQiOiJhbGwifQ.ARTrpEjqAws2LEpLdBTG8s25HuvcMMk6qhNY7aLxEuI';
+  const Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlzIjoiY2hhdmVQYXNzZSIsImtleSI6IjY1MmQ3MDA2LTgwMjctNDM2Ni05MWQ1LTk2Njk0NjkxMWRlMCIsImlhdCI6MTcxMDMzNTE5NCwiZXhwIjozMjg4MjE1MTk0LCJhdWQiOiJhbGwifQ.hscnU0FSJCuy9QSyRgSygBd_stTsP7UtCW-dUTpKWyU';
+
   const urlSelecionada = 'url1';
 
   const config = {
-      headers: {
-        'Authorization': `${Authorization}`
-      }
+    headers: {
+      'Authorization': `${Authorization}`
+    }
   };
 
   let url;
 
   if (urlSelecionada === 'url1') {
-      url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/usuario?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/usuario?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
   } else if (urlSelecionada === 'url2') {
-      url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/grupoFamiliar?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/grupoFamiliar?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
   } else if (urlSelecionada === 'url3') {
-      url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/sistema?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/sistema?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
   }
 
   // Exibir a URL na tela
