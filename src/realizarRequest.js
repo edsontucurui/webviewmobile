405a1b85c76b90b02d23d17bc948e162d4163b1c
edsontucurui <edsonsilvabsb@yahoo.com.br>
2024-03-13 15:52:36 -0300

    volta de commit

--- src/realizarRequest.js
+++ src/realizarRequest.js
@@ -1,21 +1,30 @@
-
-import { chavePasse } from './extrairChavePasse.js';
-
 function realizarRequest() {
   const instanciaApp = '1';
+  const chavePasse = document.getElementById('chavePasseInput').value;
   const chaveFuncionalidade = '731bd214-9de0-4b0c-9d63-e549296552f3';
   const Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlzIjoiY2hhdmVQYXNzZSIsImtleSI6IjY1MmQ3MDA2LTgwMjctNDM2Ni05MWQ1LTk2Njk0NjkxMWRlMCIsImlhdCI6MTcxMDMzNTE5NCwiZXhwIjozMjg4MjE1MTk0LCJhdWQiOiJhbGwifQ.hscnU0FSJCuy9QSyRgSygBd_stTsP7UtCW-dUTpKWyU';
 
-  const url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/usuario?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`; // url usuário logado
-// const url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/grupoFamiliar?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`; // url grupo familiar
-// const url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/sistema?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`; // url dados sistema usuário
-
+  const urlSelecionada = 'url1';
 
   const config = {
     headers: {
       'Authorization': `${Authorization}`
     }
   };
+
+  let url;
+
+  if (urlSelecionada === 'url1') {
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/usuario?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+  } else if (urlSelecionada === 'url2') {
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/grupoFamiliar?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+  } else if (urlSelecionada === 'url3') {
+    url = `https://api.mosiaomnichannel.com.br/clientes/chavePasse/sistema?instanciaApp=${instanciaApp}&chavePasse=${chavePasse}&chaveFuncionalidade=${chaveFuncionalidade}`;
+  }
+
+    // Exibir a URL na tela
+    const urlDisplay = document.getElementById('urlDisplay');
+    urlDisplay.innerText = `URL para request: ${url}`;
 
   fetch(url, config)
     .then(response => response.json())
@@ -27,6 +36,7 @@ function realizarRequest() {
       console.error('Ocorreu um erro:', error);
     });
 }
+
 
 function exibirChaveUnica(data) {
   const chaveUnica = data.data.chaveUnica;
