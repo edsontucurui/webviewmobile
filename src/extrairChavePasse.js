a19c8cfd94b41f8abfd47449ae177132954b21f6
edsontucurui <edsonsilvabsb@yahoo.com.br>
2024-03-13 14:10:01 -0300

    Revert "ajuste teste extrairchave"
    This reverts commit 487266e688a3771502a0727655d0962d5a945bf4.

--- src/extrairChavePasse.js
+++ src/extrairChavePasse.js
@@ -1,22 +1,22 @@
 // Extrai o valor do parâmetro "chavePasse" da URL
-const urlParams = new URLSearchParams(window.location.search);
-const chavePasse = urlParams.get('chavePasse');
+// const urlParams = new URLSearchParams(window.location.search);
+// const chavePasse = urlParams.get('chavePasse');
 
 // // Exibe o valor na tela
-const resultadoDiv = document.getElementById('resultado');
-resultadoDiv.innerHTML = chavePasse;
+// const resultadoDiv = document.getElementById('resultado');
+// resultadoDiv.innerHTML = chavePasse;
 
 // // Exporta a chavePasse para ser usada em outros arquivos
-export { chavePasse };
+// export { chavePasse };
 
 
-// function extrairChavePasse() {
-//   // Simulando a extração do parâmetro chavePasse da URL
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const chavePasse = urlParams.get('chavePasse');
-//   // const resultadoDiv = document.getElementById('resultado');
-//   // resultadoDiv.innerHTML = chavePasse;
-//   return chavePasse;
-// }
+function extrairChavePasse() {
+  // Simulando a extração do parâmetro chavePasse da URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const chavePasse = urlParams.get('chavePasse');
+  const resultadoDiv = document.getElementById('resultado');
+  resultadoDiv.innerHTML = chavePasse;
+  return chavePasse;
+}
 
-// export { extrairChavePasse };
+export { extrairChavePasse };
