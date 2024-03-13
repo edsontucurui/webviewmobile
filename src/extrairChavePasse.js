46ac909b5db15742b127499debcccc120255fe0d
edsontucurui <edsonsilvabsb@yahoo.com.br>
2024-03-13 16:11:13 -0300

    ajuste variavel extrairChavePasse

--- src/extrairChavePasse.js
+++ src/extrairChavePasse.js
@@ -1,21 +1,11 @@
-// Extrai o valor do parâmetro "chavePasse" da URL
+// Extrai o valor do parâmetro "chavepasse" da URL
 const urlParams = new URLSearchParams(window.location.search);
 const chavePasse = urlParams.get('chavePasse');
 
-// // Exibe o valor na tela
+// Exibe o valor na tela
 const resultadoDiv = document.getElementById('resultado');
 resultadoDiv.innerHTML = chavePasse;
+
+
 // // Exporta a chavePasse para ser usada em outros arquivos
 // export { chavePasse };
-
-
-// function extrairChavePasse() {
-//   // Simulando a extração do parâmetro chavePasse da URL
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const chavePasse = urlParams.get('chavePasse');
-//   const resultadoDiv = document.getElementById('resultado');
-//   resultadoDiv.innerHTML = chavePasse;
-//   return chavePasse;
-// }
-
-// export { extrairChavePasse };
